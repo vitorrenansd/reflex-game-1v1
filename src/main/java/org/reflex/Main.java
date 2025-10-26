d2a7c314f36d8f53f77fce682f4cc690ad6e1dbd
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-25 22:27:11 -0300

    update: added tests to Game class

--- src/main/java/org/reflex/Main.java
+++ src/main/java/org/reflex/Main.java
@@ -2,5 +2,11 @@ package org.reflex;
 
 public class Main {
 	public static void main(String[] args) {
+		Game g = new Game();
+
+		g.setMinTime(1);
+		g.setMaxTime(5);
+		
+		g.testReactionTime();
 	}
 }
